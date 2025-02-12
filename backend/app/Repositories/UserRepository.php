a580952db4df6f25b4d17ddf23a12b1c1f61be63
MyatPaingMin <myatpaingminn@gmail.com>
2025-02-12 20:38:43 +0700

    fix: login feature

--- backend/app/Repositories/UserRepository.php
+++ backend/app/Repositories/UserRepository.php
@@ -24,6 +24,6 @@ class UserRepository extends BaseRepository
     public function findByEmail($email)
     {
         return $this->model()::where('email', $email)->first(); // laravel eloquent
-        // return DB::raw("SELECT * FROM users WHERE email = $email LIMIT 1 "); // raw query
+        // return DB::raw("SELECT * FROM users WHERE email = $email LIMIT 1 "); // raw query 
     }
 }

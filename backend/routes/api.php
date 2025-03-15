50a655e54318e8f314959c06bb8bdc3622210f18
zawwanahtue <yaza9036@gmail.com>
2025-03-15 11:51:19 +0630

    Add more user detail to edit and split password edit api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -52,6 +52,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::delete('/delete-photo', [UserController::class, 'deleteUserPhoto']);
         Route::patch('/edit-detail', [UserController::class, 'updateProfile']);
         Route::get('/active-user-list', [UserController::class, 'getUserList']);
+        Route::patch('/update-password', [UserController::class, 'updatePassword']);
     });
     Route::post('/logout', [UserController::class, 'logout']);
     // User routes

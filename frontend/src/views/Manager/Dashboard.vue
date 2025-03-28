4136ead8e6bef21b5121a813862ea46e083d78a7
Htet Wai Yan <hwy.dev@gmail.com>
2025-03-28 12:47:16 +0700

    wip: dashboard

--- frontend/src/views/Manager/Dashboard.vue
+++ frontend/src/views/Manager/Dashboard.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import Layout from '@/components/ui/Layout.vue'
+import ArticleChart from '@/components/pagespecific/coordinator-home/ArticleChart.vue'
+import GuestListTable from '@/components/pagespecific/coordinator-home/GuestListTable.vue'
+import MagazineArticles from '@/components/pagespecific/coordinator-home/MagazineArticles.vue'
+
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import Badge from '@/components/ui/badge/Badge.vue'
 import { ArrowUp, ArrowDown } from 'lucide-vue-next'
 import { ref } from 'vue'
 
 const daysLeft = ref(4)
+
+const articleStats = [
+  { label1: 'Total Articles', value1: '132 nos', label2: 'Published Articles', value2: '100 nos' },
+  { label1: 'Reviewed Articles', value1: '32 nos', label2: 'Approved', value2: '100 nos' },
+  { label1: 'Pending Review', value1: '100 nos', label2: 'Reject', value2: '32 nos' },
+]
 </script>
 
 <template>
@@ -27,53 +37,84 @@ const daysLeft = ref(4)
               'bg-transparent text-gray-700': daysLeft >= 10,
             }"
             class="px-4 py-2"
-            >TOTAL DATE LEFT FOR PUBLISH: {{ daysLeft }} Days</Badge
+            >TOTAL DAYS LEFT UNTIL PUBLICATIONS: {{ daysLeft }} Days</Badge
           >
         </div>
       </div>
 
       <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <Card class="bg-primary text-white">
+        <Card class="bg-primary text-white md:col-span-2">
           <CardHeader>
-            <CardTitle>Articles (Pre Submission)</CardTitle>
+            <CardTitle class="font-primary text-lg">Articles</CardTitle>
           </CardHeader>
-          <CardContent class="space-y-2">
-            <p>Total Articles: <strong>132</strong></p>
-            <p>Review Articles: <strong>100</strong></p>
-            <p>Under Review: <strong>32</strong></p>
+          <CardContent class="font-primary grid grid-cols-1 lg:grid-cols-2 gap-x-12 gap-y-3">
+            <div class="flex flex-col gap-3">
+              <div class="flex items-center justify-between">
+                <div class="text-accent">Total Articles</div>
+                <div class="text-lg">132</div>
+              </div>
+
+              <div class="flex items-center justify-between">
+                <div class="text-accent">Review Articles</div>
+                <div class="text-lg">100</div>
+              </div>
+
+              <div class="flex items-center justify-between">
+                <div class="text-accent">Under Review</div>
+                <div class="text-lg">32</div>
+              </div>
+            </div>
+            <div class="flex flex-col gap-3">
+              <div class="flex items-center justify-between">
+                <div class="text-accent">Published Articles</div>
+                <div class="text-lg">100</div>
+              </div>
+
+              <div class="flex items-center justify-between">
+                <div class="text-accent">Approved</div>
+                <div class="text-lg">100</div>
+              </div>
+
+              <div class="flex items-center justify-between">
+                <div class="text-accent">Rejected</div>
+                <div class="text-lg">100</div>
+              </div>
+            </div>
           </CardContent>
         </Card>
 
         <Card class="bg-primary text-white">
           <CardHeader>
-            <CardTitle>Articles (Final)</CardTitle>
+            <CardTitle class="font-primary text-lg">Current Academic Year</CardTitle>
           </CardHeader>
-          <CardContent class="space-y-2">
-            <p>Total Articles: <strong>132</strong></p>
-            <p>Approved: <strong>100</strong></p>
-            <p>Reject: <strong>32</strong></p>
-          </CardContent>
-        </Card>
+          <CardContent class="font-primary flex flex-col gap-3">
+            
+            <div class="flex items-center justify-between">
+              <div class="text-accent">Participate </div>
+              <div class="text-lg">20% <ArrowUp class="inline w-4 h-4 text-green-500" /></div>
+            </div>
 
-        <Card class="bg-primary text-white">
-          <CardHeader>
-            <CardTitle>Current Academic Year</CardTitle>
-          </CardHeader>
-          <CardContent class="space-y-2">
-            <p>
-              Participate Rate: <strong>20%</strong>
-              <ArrowUp class="inline w-4 h-4 text-green-500" />
-            </p>
-            <p>
-              Interest Rate: <strong>10%</strong>
-              <ArrowUp class="inline w-4 h-4 text-green-500" />
-            </p>
-            <p>
-              In Time Rate: <strong>5%</strong>
-              <ArrowDown class="inline w-4 h-4 text-red-500" />
-            </p>
+            <div class="flex items-center justify-between">
+              <div class="text-accent">Interest Rate </div>
+              <div class="text-lg">10% <ArrowUp class="inline w-4 h-4 text-green-500" /></div>
+            </div>
+
+            <div class="flex items-center justify-between">
+              <div class="text-accent">In Time Rate </div>
+              <div class="text-lg">10% <ArrowDown class="inline w-4 h-4 text-red-500" /></div>
+            </div>
+
           </CardContent>
         </Card>
+      </div>
+
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div>
+          <ArticleChart />
+          <GuestListTable class="mt-6" />
+        </div>
+
+        <MagazineArticles />
       </div>
     </div>
   </Layout>
